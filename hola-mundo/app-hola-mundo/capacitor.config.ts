62eb9891300c02bc5f6c11f686205ab5d54cc468
pablochavez02 <141696292+pablochavez02@users.noreply.github.com>
2023-08-27 23:34:50 -0600

    app hola mundo

---
+++ hola-mundo/app-hola-mundo/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-hola-mundo',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
